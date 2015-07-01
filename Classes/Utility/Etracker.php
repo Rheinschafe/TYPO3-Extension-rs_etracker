18a6089def95b7b6de098f3b212ab89bd943b7b5
Daniel Hürtgen <daniel@higidi.com>
2015-07-01 13:54:31 +0200

    [FEATURE] etracker class implemented

---
+++ Classes/Utility/Etracker.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Rheinschafe\RsEtracker\Utility;
+
+/*
+ * This file is part of the Rheinschafe/Etracker project under GPLv2 or later.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ */
+
+use etracker as BaseEtracker;
+
+/**
+ * Etracker.
+ *
+ * @package    rs_etracker
+ * @subpackage Utility
+ * @author     Daniel Hürtgen <huertgen@rheinschafe.de>
+ */
+class Etracker extends BaseEtracker {
+}
