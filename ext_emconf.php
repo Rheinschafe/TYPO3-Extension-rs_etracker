9adfe12262da589df170b3c2e10e1b59dc5b36ab
Daniel Hürtgen <daniel@higidi.com>
2016-04-08 16:05:44 +0200

    [FEATURE] compatibility fix for TYPO3 7.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,10 +31,18 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints'                   => array(
 		'depends'   => array(
 			'php'   => '5.3.7-0.0.0',
-			'typo3' => '6.0.0-6.2.99',
+			'typo3' => '6.0.0-7.99.99',
 		),
 		'conflicts' => array(),
 		'suggests'  => array(),
+	),
+	'autoload'                      => array(
+		'psr-4'    => array(
+			'Rheinschafe\\RsEtracker\\' => 'Classes',
+		),
+		'classmap' => array(
+			'Resources/PHP/Etracker',
+		),
 	),
 	'_md5_values_when_last_written' => '',
 );
